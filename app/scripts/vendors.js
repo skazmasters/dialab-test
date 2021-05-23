0f39a890a275004dff1fb0319fcd0fedcf53716c
Eugene <eugene.kotsarev@gmail.com>
2021-05-23 23:43:20 +0300

    upd vue templates, add momentjs, refactor vendors

--- app/scripts/vendors.js
+++ app/scripts/vendors.js
@@ -8,24 +8,30 @@ import lazySizes from 'lazysizes';
 import 'lazysizes/plugins/native-loading/ls.native-loading';
 import 'lazysizes/plugins/object-fit/ls.object-fit';
 // import svgPolyfill from 'svg4everybody';
-import jquery from 'jquery';
-import swiper from 'swiper';
-import imask from 'imask';
-import datepicker from 'js-datepicker';
-import { format as dateFormat } from 'date-fns';
-import select2 from 'select2';
-import PerfectScrollbar from 'perfect-scrollbar';
+// import jquery from 'jquery';
+// import swiper from 'swiper';
+// import imask from 'imask';
+// import datepicker from 'js-datepicker';
+// import { format as dateFormat } from 'date-fns';
+// import select2 from 'select2';
+// import PerfectScrollbar from 'perfect-scrollbar';
 
-window.$ = window.jQuery = jquery;
+// window.$ = window.jQuery = jquery;
 // window.svg4everybody = svgPolyfill;
-window.Swiper = swiper;
-window.IMask = imask;
-window.select2 = select2;
-window.PerfectScrollbar = PerfectScrollbar;
-window.datepicker = datepicker;
-window.dateHelper = {
-  format: dateFormat,
-};
+// window.Swiper = swiper;
+// window.IMask = imask;
+// window.select2 = select2;
+// window.PerfectScrollbar = PerfectScrollbar;
+// window.datepicker = datepicker;
+// window.dateHelper = {
+//   format: dateFormat,
+// };
+
+import moment from 'moment';
+import 'moment/locale/ru';
+
+window.moment = moment;
+moment.locale('ru');
 
 lazySizes.cfg.lazyClass = 'lazy';
 lazySizes.cfg.srcAttr = 'data-original';
